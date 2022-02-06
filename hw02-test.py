0239bc054699e4eb4553c4423c346f9938fab75f
Nicholas Beninato <beninato8@gmail.com>
2022-02-06 17:02:57 -0600

    build before running and show specific issues with output

--- hw02-test.py
+++ hw02-test.py
@@ -1,6 +1,7 @@
 from subprocess import Popen, PIPE
 import json
 from tqdm import tqdm
+import difflib
 
 TEST_FILE = 'tests.json'
 
@@ -8,8 +9,14 @@ with open(TEST_FILE, 'r') as f:
     tests = json.load(f)
 
 def shell(cmd, stdin=None):
-    out, err = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, stdin=PIPE).communicate(input=stdin.encode())
+    p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, stdin=PIPE)
+    out, err = p.communicate(input=stdin.encode() if stdin else None)
     return out.decode('utf8').strip(), err.decode('utf8').strip()
+
+print('Building:')
+buildOutput, buildError = shell('make build')
+print(buildOutput)
+print(buildError)
 
 for _, testCase in tqdm(tests.items()):
     userOutput, userError = shell('make run', stdin=testCase['input'])
@@ -19,6 +26,14 @@ for _, testCase in tqdm(tests.items()):
         print(f"Expected output:\n{testCase['output']}\n")
         print(f"Program output:\n{userOutput}\n")
         print(f"Program error:\n{userError}")
+        print('Difference between user and expected outputs:')
+        for i,s in enumerate(difflib.ndiff(userOutput, testCase['output'])):
+            if s[0]==' ': continue
+            elif s[0]=='-':
+                print(u'Delete "{}" from position {}'.format(s[-1],i))
+            elif s[0]=='+':
+                print(u'Add "{}" to position {}'.format(s[-1],i))    
+        print()   
         exit()
 
 # print(tests)
